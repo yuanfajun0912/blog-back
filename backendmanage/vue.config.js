83848c202a859d89bc24f705ce4c434ebd57801f
yuanfajun0912 <1654891661@qq.com>
2020-05-30 19:00:06 +0800

    refresh

--- backendmanage/vue.config.js
+++ backendmanage/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // npm run build 后生成的打包文件放在哪里
   outputDir: __dirname + '/../server/back',
   // publicPath: './',  //解决打包后页面空白的问题
-  publicPath: process.env.NODE_ENV === 'production'  // 生产/开发环境的路径问题
-    ? '/back/'
-    : '/'
+  // publicPath: process.env.NODE_ENV === 'production'  // 生产/开发环境的路径问题
+  //   ? '/back/'
+  //   : '/'
 }
